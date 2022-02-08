1ed3ec282227c9dfbf8056764e043d48f506b1e1
Ben <b.fowl87@gmail.com>
2022-02-08 08:48:30 +0000

    Gem setup and application setup with tailwind
    - nomics (Personal ruby gem for project) Wrapper around Nomics API
    - pry-rails (Debugging)
    - rspec-rails (Testing)
    - view_component (Web components from github
    - webmock (Stubbing library for HTTP requests)
    - dotenv (ENV var manager)

---
+++ config/initializers/draw_routes.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class ActionDispatch::Routing::Mapper
+  def draw(route_name)
+    instance_eval(File.read(Rails.root.join("config/routes/#{route_name}.rb")))
+  end
+end
---
+++ config/routes/marketing_routes.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+resources :tickers, except: [:update, :edit]
